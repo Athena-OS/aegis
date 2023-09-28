fa7ab673e5b9f9e06f20175f114d88351ed462c4
D3vil0p3r <>
2023-09-28 11:37:27 +0200

    Some fixes

--- src/internal/returncode_eval.rs
+++ src/internal/returncode_eval.rs
@@ -23,6 +23,7 @@ pub fn files_eval(return_code: std::result::Result<(), std::io::Error>, logmsg: 
             log::info!("{}", logmsg);
         }
         Err(e) => {
+            log::info!("{} ERROR: {}", logmsg, e);
             crash(
                 format!("{} ERROR: {}", logmsg, e),
                 return_code.unwrap_err().raw_os_error().unwrap(),
