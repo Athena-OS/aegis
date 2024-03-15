e01ea170f9662f546ca2ee26ab25840b030e8824
D3vil0p3r <crazysnob@live.it>
2024-03-15 22:57:08 +0100

    Update users.rs

--- aegis-arch/src/functions/users.rs
+++ aegis-arch/src/functions/users.rs
@@ -8,12 +8,15 @@ use std::process::Command;
 
 pub fn new_user(username: &str, hasroot: bool, password: &str, do_hash_pass: bool, shell: &str) {
     let shell: &str = shell;
+    let mut _password = String::new();
+    // Username cannot contain any space
+    let sanitized_username = username.replace(' ', "");
     if do_hash_pass {
-        let hashed_pass = &*hash_pass(password).stdout;
-        let _password = match std::str::from_utf8(hashed_pass) {
-            Ok(v) => v,
-            Err(e) => panic!("Failed to hash password, invalid UTF-8 sequence {}", e),
-        };
+        let hashed_pass = hash_pass(password).stdout;
+        _password = String::from_utf8_lossy(&hashed_pass).into_owned();
+    }
+    else {
+        _password = password.to_string();
     }
     let shell_to_install = match shell {
         "bash" => "bash",
@@ -40,11 +43,11 @@ pub fn new_user(username: &str, hasroot: bool, password: &str, do_hash_pass: boo
                 String::from("-s"),
                 String::from(shell_path),
                 String::from("-p"),
-                String::from(password).replace('\n', ""),
-                String::from(username),
+                String::from(_password).replace('\n', ""),
+                String::from(sanitized_username),
             ],
         ),
-        format!("Create user {}", username).as_str(),
+        format!("Create user {}", sanitized_username).as_str(),
     );
     if hasroot {
         exec_eval(
@@ -53,10 +56,10 @@ pub fn new_user(username: &str, hasroot: bool, password: &str, do_hash_pass: boo
                 vec![
                     String::from("-aG"),
                     String::from("wheel,rfkill,sys,lp,input"),
-                    String::from(username),
+                    String::from(sanitized_username),
                 ],
             ),
-            format!("Add user {} to wheel group", username).as_str(),
+            format!("Add user {} to wheel group", sanitized_username).as_str(),
         );
         files_eval(
             files::sed_file(
@@ -74,13 +77,13 @@ pub fn new_user(username: &str, hasroot: bool, password: &str, do_hash_pass: boo
             files::create_directory("/mnt/var/lib/AccountsService/users/"),
             "Create /mnt/var/lib/AccountsService",
         );
-        files::create_file(&format!("/mnt/var/lib/AccountsService/users/{}", username));
+        files::create_file(&format!("/mnt/var/lib/AccountsService/users/{}", sanitized_username));
         files_eval(
             files::append_file(
-                &format!("/mnt/var/lib/AccountsService/users/{}", username),
+                &format!("/mnt/var/lib/AccountsService/users/{}", sanitized_username),
                 "[User]\nSession=gnome-xorg\nIcon=/usr/share/pixmaps/faces/hackmyavatar.jpg",
             ),
-            format!("Populate AccountsService user file for {}", username).as_str(),
+            format!("Populate AccountsService user file for {}", sanitized_username).as_str(),
         )
     }
 }
